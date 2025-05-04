ec254b7fe71dd1e88b4442234b57ab6f1d479d54
FNHENRIQUES <nandoje@hotmail.com>
2025-05-03 21:49:16 -0300

    week2Commit

--- server.js
+++ server.js
@@ -6,21 +6,34 @@
  * Require Statements
  *************************/
 const express = require("express")
+const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
 
 /* ***********************
+ * /view Engine and Template
+ *************************/
+app.set("view engine", "ejs")
+app.use(expressLayouts)
+app.set("layout", "./layouts/layout") // not at views root
+
+/* ***********************
  * Routes
  *************************/
 app.use(static)
+// Index Route
+app.get("/", (req, res) => {
+  res.render("index", { title: "Home" })
+})
 
 /* ***********************
  * Local Server Information
  * Values from .env (environment) file
  *************************/
-const port = process.env.PORT
-const host = process.env.HOST
+const port = process.env.PORT||5500; 
+const host = process.env.HOST||"localhost"; // Default to localhost if not set in .env
+
 
 /* ***********************
  * Log statement to confirm server operation
